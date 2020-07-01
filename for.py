1816828b8bf8c4519cc7cd236e7f6064e3768c1b
dreamer-1996 <nairitbandyopadhyay@gmail.com>
2020-07-01 13:39:03 +0530

    Added for.py

---
+++ for.py
@@ -0,0 +1,13 @@
+
+#Example of range
+for i in range(1,11,2):
+    print ('Hello this is Nairit '+ str(i))
+    
+#Example of negative range
+    
+for i in range(20,10,-1):
+     print ('Hello this is Nairit '+ str(i))
+
+#Example of list range
+for x in ["Apples","Banana","Guava"]:
+    print(x)
