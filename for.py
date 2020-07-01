26a9ec8686ab957937e6ef16c6ec3d12f5825d71
dreamer-1996 <61982054+dreamer-1996@users.noreply.github.com>
2020-07-01 14:30:49 +0530

    Update for.py
    Some privacy related issues

--- for.py
+++ for.py
@@ -1,12 +1,12 @@
 
 #Example of range
 for i in range(1,11,2):
-    print ('Hello this is Nairit '+ str(i))
+    print ('Hello this is Me '+ str(i))
     
 #Example of negative range
     
 for i in range(20,10,-1):
-     print ('Hello this is Nairit '+ str(i))
+     print ('Hello this is Me '+ str(i))
 
 #Example of list range
 for x in ["Apples","Banana","Guava"]:
